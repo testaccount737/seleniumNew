bf64c1b02c5587d718f9cb2bd641fb28b1b7aa71
testaccount737 <neon_ari@rediffmail.com>
2025-07-21 01:11:39 -0500

    Create HomeTest.java
    user directory

---
+++ src/test/java/org/java/selenium/HomeTest.java
@@ -0,0 +1,37 @@
+package org.java.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
+import java.util.concurrent.TimeUnit;
+
+public class HomeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\resource\\chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			
+			driver.navigate().to("https://www.google.com");
+			driver.manage().window().maximize();
+			
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			
+			WebElement searchBox =  driver.findElement(By.name("q"));
+			searchBox.sendKeys("selenium");
+			searchBox.sendKeys(Keys.RETURN);
+			
+			
+			
+			driver.quit();
+			
+			
+		
+	}
+
+}
